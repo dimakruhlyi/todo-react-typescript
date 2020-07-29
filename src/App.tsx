4b5e90dfe9c39cf428a9c506b1968287c4ac96f2
dimakruhlyi <dimakruhly@gmail.com>
2020-07-29 23:17:27 +0300

    created todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React, {useState} from 'react';
 import { Navbar } from './components/Navbar';
 import {TodoForm} from './components/TodoForm';
+import { TodoList } from './components/TodoList';
+import { ITodo } from './interfaces';
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
   const AddHandler = (title: string) => {
-    console.log('Add new ToDo ', title);
+    const newTodo: ITodo = {
+      title: title,
+      id: Date.now(),
+      completed: false
+    }
+    //setTodos([newTodo, ...todos])
+    setTodos(prev => {
+      return [newTodo, ...prev]
+    })
   }
 
   return (
@@ -14,7 +24,7 @@ function App() {
       <Navbar />
       <div className="container">
         <TodoForm onAdd = {AddHandler}/>
-
+        <TodoList todos = {todos}/>
       </div>
     </React.Fragment>
     
