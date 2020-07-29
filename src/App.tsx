36f95b7314eed728350dc98a8b302d5fffe917ed
dimakruhlyi <dimakruhly@gmail.com>
2020-07-29 20:25:12 +0300

    transfer parametrs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Navbar } from './components/Navbar';
 import {TodoForm} from './components/TodoForm';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const AddHandler = (title: string) => {
+    console.log('Add new ToDo ', title);
+  }
+
   return (
     <React.Fragment>
       <Navbar />
       <div className="container">
-        <TodoForm />
+        <TodoForm onAdd = {AddHandler}/>
 
       </div>
     </React.Fragment>
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,13 +1,28 @@
-import React, {useState} from 'react';
+import React, {useRef} from 'react';
 
-export const TodoForm: React.FC = () => {
-    const [title, setTitle] = useState<string>('')
-    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTitle(event.target.value)
+interface TodoFormProps{
+    onAdd(title: string): void
+}
+
+export const TodoForm: React.FC<TodoFormProps> = (props) => {
+    // const [title, setTitle] = useState<string>('')
+    const ref = useRef<HTMLInputElement>(null);
+
+    // const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     setTitle(event.target.value)
+    // }
+
+    const keyPressHandler = (event: React.KeyboardEvent) => {
+        if(event.key === 'Enter'){
+            props.onAdd(ref.current!.value);
+            ref.current!.value = '';
+            // console.log(title);
+            // setTitle('');
+        }
     }
     return(
         <div className="input-field mt2">
-            <input type="text" value = {title} onChange = {changeHandler} id = "title" placeholder = "Input ToDo name"/>
+            <input type="text" /*value = {title} onChange = {changeHandler} */ ref = {ref} onKeyPress = {keyPressHandler} id = "title" placeholder = "Input ToDo name"/>
             <label htmlFor="title" className = "active">Input ToDo name</label>
         </div>
     )
